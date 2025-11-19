e74b2a964d7286727c8331f1c82a466924c4b83c
kulatarin <kulatarin15@gmail.com>
2025-11-19 18:17:24 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,36 @@ function applyGlobalSettings() {
 
     // Apply font
     document.body.style.fontFamily = `'${font}', sans-serif`;
+var express = require('express');
+var request = require('request');
 
+var router = express.Router();
+
+router.post("/", async (req, res, next) => {
+  try {
+    const translated = await translate(req.query.source, 'en', req.query.content);
+    const result = await translate('en', req.query.target, translated); 
+    // presume you intend on returning something from your API?
+    return res.json({ result });
+  } catch (e) {
+    return next(e);
+  }
+})
+
+const translate = (source, target, content) => 
+  new Promise((resolve, reject) => 
+    request(`https://api-platform.systran.net/translation/text/translate?input=${content}&source=${source}&target=${target}&key=xxxxxxxx-783f-4f90-aea4-7fb357016647`, (err, data, body) => {
+      if (err) return reject(err);
+
+      try {
+        return resolve(JSON.parse(body));
+      } catch (e) {
+        return reject(e);
+      }
+    })
+  );
+
+module.exports = router;
     // Apply theme
     switch (theme) {
         case "dark":
@@ -120,3 +149,4 @@ if (window.location.pathname.includes("settings.html")) {
 
     loadSettings();
 }
+
